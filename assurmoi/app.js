6d1021bace884bbc261f8043a4d324d5413590ed
Pajot Julien <jujupajot85@gmail.com>
2026-03-23 14:48:24 +0100

    feat: add user routes get and post

--- assurmoi/app.js
+++ assurmoi/app.js
@@ -3,6 +3,34 @@
     require('dotenv').config()
 
     const PORT = process.env.PORT || 3000
+
+    app.use(express.json())
+    
+    app.post('/user/',(req, res)=> {
+        const user = req.body
+        res.status(201).json({
+            user: user 
+        })
+    })
+
+    app.get('/user/:id',(req,res, next)=>{
+        res.status(200).json ({
+            user: req.params.id
+        })
+    })
+
+    app.get ('/',(req, res,next)=> {
+        console.log('middleware Homepage')
+        next()
+    },(req, res,next)=>{
+        console.log('Controller Homepage')
+        res.status(200).json({
+            message: "Bienvenu sur la page d'accueil"
+        })
+    })
+
+
+
     app.listen(PORT, ()=>{
         console.log('server running on port',PORT)
     })
