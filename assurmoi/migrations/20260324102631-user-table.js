949f633490c698b557ba9a7346c16b933211614f
Pajot Julien <jujupajot85@gmail.com>
2026-03-25 16:04:59 +0100

    feat: add migration to database

--- assurmoi/migrations/20260324102631-user-table.js
+++ assurmoi/migrations/20260324102631-user-table.js
@@ -3,37 +3,40 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('user', {
       id: {
         type: Sequelize.INTEGER,
-        autoIncrement: true,
         primaryKey: true,
-        allowNull: false
+        autoIncrement: true,
+        allowNull: false,
       },
       username: {
-        type: Sequelize.STRING(50),
-        allowNull: false
-      },
-      password: {
         type: Sequelize.STRING,
-        allowNull: false
-      },
-      firstname: {
-        type: Sequelize.STRING(50),
-        allowNull: true
-      },
-      lastname: {
-        type: Sequelize.STRING(50),
-        allowNull: true
+        allowNull: false,
+        unique: true,
       },
       email: {
         type: Sequelize.STRING,
-        allowNull: true
-      }
+        allowNull: false,
+        unique: true,
+      },
+      password: { type: Sequelize.STRING, allowNull: false },
+      firstname: { type: Sequelize.STRING, allowNull: true },
+      lastname: { type: Sequelize.STRING, allowNull: true },
+      role: {
+        type: Sequelize.ENUM('ADMIN', 'AGENT', 'EXPERT', 'CLIENT'),
+        allowNull: false,
+        defaultValue: 'CLIENT',
+      },
+      token: { type: Sequelize.TEXT, allowNull: true },
+      refresh_token: { type: Sequelize.TEXT, allowNull: true },
+      two_step_code: { type: Sequelize.STRING, allowNull: true },
+      active: { type: Sequelize.BOOLEAN, defaultValue: true },
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
-  }
+    await queryInterface.dropTable('user');
+    await queryInterface.sequelize.query('DROP TYPE IF EXISTS "enum_user_role";');
+  },
 };
